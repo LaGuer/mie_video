20c02c474f1d503cc923ece6f897abd6bdd1fd03
mjo22 <michaelobrien@nyu.edu>
2019-01-24 20:27:21 -0500

    Fix for restructuring

--- __init__.py
+++ __init__.py
@@ -1,4 +1,6 @@
-__all__ = ['fitting', 'editing']
+__all__ = ['fitting', 'utilities', 'tracking',
+           'animation', 'editing', 'h5video',
+           'circletransform']
 
-from . import fitting
-from . import editing
+from . import fitting, utilities, tracking, animation
+from utilities import editing, circletransform, h5video
